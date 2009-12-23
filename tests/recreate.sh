c2e3fca8306ea0e4f843eb4d221fbb08f1e198ad
Rahul Kumar <rahulbenegal@gmail.com>
2009-12-23 18:23:50 +0530

    helps recreate a test case from broken one
    test cases are failing due to formatting/cosmetic changes
    in listing (e.g coloring) and i use a list after most operations
    to check previous test.

---
+++ tests/recreate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#*******************************************************#
+#                      x.sh                             #
+#                 written by Rahul Kumar                #
+#                    2009/12/01                         #
+#                 Does ...                              #
+#*******************************************************#
+
+oldfile="$1"
+[ ! -f "$oldfile" ] && { echo "Could not find $oldfile"; exit -1 ; }
+
+LOADSTR=
+if grep -q CATEOF "$oldfile"; then
+   sed -n '/<<CATEOF/,/^CATEOF/p' "$oldfile" | grep -v "CATEOF" > data.1
+   LOADSTR='--load data.1'
+   echo found data, saved as data.1
+   wc -l data.1
+fi
+str=$( echo "$oldfile" | sed 's/^t[0-9]*-//;s/.sh$//' )
+echo "Using suffix:$str"
+read
+grep '^>>> ' "$oldfile" | sed 's/^>>> //' 
+read
+grep '^>>> ' "$oldfile" | sed 's/^>>> //' | ./rtest2.sh $LOADSTR "$str"
+
+echo
+echo renaming old file
+mv "$oldfile" O$oldfile
